99fdb725935a0c2755c98e0826ea509c57cb1727
Fábio Ferreira <ffcfpten@gmail.com>
2019-09-16 22:24:16 +0100

    feat: remove placeholder data

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -8,33 +8,6 @@ const Home = () => {
   const [data, setData] = useState()
 
   useEffect(() => { // Component Did Mount
-    //DEBUG
-    setData({
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'Weekly Active Users',
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: 'rgba(75,192,192,0.4)',
-          borderColor: 'rgba(75,192,192,1)',
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'rgba(75,192,192,1)',
-          pointBackgroundColor: '#fff',
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-          pointHoverBorderColor: 'rgba(220,220,220,1)',
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: [65, 59, 80, 81, 56, 55, 40],
-        }
-      ]
-    })
     // Remove when done above
     fetch(`${process.env.REACT_APP_APISCHEMA}://${process.env.REACT_APP_APIHOST}:${process.env.REACT_APP_APIPORT}/analytics/wau`)
       .then((res) => res.json())
